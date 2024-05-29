fcf75219a977ecae4f28797c6e03dc8acdcab8d6
bmccarson <brian@mccarson.us>
2024-05-28 20:35:44 -0400

    update Decimal struct with subunits and percision

--- money/decimal.go
+++ money/decimal.go
@@ -1,5 +1,10 @@
 package money
 
-// Decimal is capable of storing a floating-point value
+// Decimal can represent a floating-point number with a fixed precision.
+// example: 1.52 = 152 * 10^(-2) will be stored as {152, 2}
 type Decimal struct {
+	//subunits is the amount of subunits. Multiply it by the precision to get the real value
+	subunits int64
+	// Number of "subunits" in a unit, expressed as a power of 10
+	precision byte
 }
