4e73245c3f7ce525e44afb80214c5bbd9a7b9740
bmccarson <brian@mccarson.us>
2024-05-31 17:14:18 -0400

    create ParseDecimal function

--- money/decimal.go
+++ money/decimal.go
@@ -1,5 +1,11 @@
 package money
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // Decimal can represent a floating-point number with a fixed precision.
 // example: 1.52 = 152 * 10^(-2) will be stored as {152, 2}
 type Decimal struct {
@@ -7,4 +13,34 @@ type Decimal struct {
 	subunits int64
 	// Number of "subunits" in a unit, expressed as a power of 10
 	precision byte
+}
+
+const (
+	// ErrInvalidDecimal is returned if the decimal is malformed.
+	ErrInvalidDecimal = Error("unable to convert the decimal")
+
+	// ErrTooLarge is returned if the quantity is too large - this would cause floating point percision errors
+	ErrTooLarge = Error("quantity over 10^12 is too large")
+)
+
+// ParseDecimal converts a string into a Decimal representation.
+// It assumes there is up to one decimal separator, and that the separator is '.'
+func ParseDecimal(value string) (Decimal, error) {
+	intPart, fracPart, _ := strings.Cut(value, ".")
+
+	// maxDecimal is the number of digits in a thousand billion.
+	const maxDecimal = 12
+
+	if len(intPart) > maxDecimal {
+		return Decimal{}, ErrTooLarge
+	}
+
+	subunits, err := strconv.ParseInt(intPart+fracPart, 10, 64)
+	if err != nil {
+		return Decimal{}, fmt.Errorf("%w: %s", ErrInvalidDecimal, err.Error())
+	}
+
+	precision := byte(len(fracPart))
+
+	return Decimal{subunits: subunits, precision: precision}, nil
 }
