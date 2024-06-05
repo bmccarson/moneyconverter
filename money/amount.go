24f8c9e0c39de9252aab83352f378122012e511d
bmccarson <brian@mccarson.us>
2024-06-04 20:34:33 -0400

    create NewAmount func to check quantity and currency precision and return Amount

--- money/amount.go
+++ money/amount.go
@@ -5,3 +5,18 @@ type Amount struct {
 	quantity Decimal
 	currency Currency
 }
+
+const (
+	// ErrTooPrecise is returned if the number is too precise for the currency.
+	ErrTooPrecise = Error("quantity is too precise")
+)
+
+// NewAmount returns an Amount of money.
+func NewAmount(quantity Decimal, currency Currency) (Amount, error) {
+	if quantity.precision > currency.precision {
+		return Amount{}, ErrTooPrecise
+	}
+	quantity.precision = currency.precision
+
+	return Amount{quantity: quantity, currency: currency}, nil
+}
